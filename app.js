eed9e7651959e6ae0b4d3a95bcb5fb760ccd1315
Prahana Mondal <mondalprahana2@gmail.com>
2026-02-26 23:45:36 +0530

    Fix rate limiter implementation in app.js

--- app.js
+++ app.js
@@ -18,17 +18,15 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 app.set('trust proxy', 1)
-app.use(
-  rateLimiter({
-    windowsMs: 15 * 60 * 1000,
+const rateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
     max: 100,
-  })
-)
+})
 app.use(express.json())
 app.use(helmet())
 app.use(cors())
 app.use(xss())
-app.use(rateLimit)
+app.use(rateLimiter)
 // extra packages
 app.get('/', (req, res) => {
   res.send('jobs api')
